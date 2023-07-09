19867c3c1f2ad4a36009e00a04330aa6c766920f
Nikita <nikitasharma2999@gmail.com>
2023-07-09 18:04:16 +0530

    update gat

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Nikita Sharma is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://nikitasharmaa.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
